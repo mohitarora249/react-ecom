4bd195328a5cec592cbc1a0ca79cbdb8472aed3d
mohitarora249 <mohitarora249@gmail.com>
2022-09-02 09:25:39 +0530

    added ProductGrid component tests

--- src/components/ProductGrid/index.jsx
+++ src/components/ProductGrid/index.jsx
@@ -2,7 +2,10 @@ import ProductCard from "../ProductCard";
 
 const ProductGrid = (props) => {
   return (
-    <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <div
+      data-testid="products-wrapper"
+      className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5"
+    >
       {props.products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
